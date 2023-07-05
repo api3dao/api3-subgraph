d1af3b377b802d24e7f9b7f1f0f4d4cfec2561b3
danielszuk <daniel.szuk@vacuumlabs.com>
2023-07-05 13:52:46 +0200

    add Withdrew event handling

--- subgraphs/api3-events/src/prepayment-depository.mapping.ts
+++ subgraphs/api3-events/src/prepayment-depository.mapping.ts
@@ -1,5 +1,8 @@
-import { Deposited as DepositedEvent } from "../types/PrepaymentDepository/PrepaymentDepository";
-import { Deposit } from "../types/schema";
+import {
+  Deposited as DepositedEvent,
+  Withdrew as WithdrewEvent,
+} from "../types/PrepaymentDepository/PrepaymentDepository";
+import { Deposit, Withdraw } from "../types/schema";
 
 export function handleDeposited(event: DepositedEvent): void {
   const entity = new Deposit(
@@ -12,6 +15,24 @@ export function handleDeposited(event: DepositedEvent): void {
   entity.amount = event.params.amount;
   entity.withdrawalLimit = event.params.withdrawalLimit;
   entity.sender = event.params.sender;
+
+  entity.save();
+}
+
+export function handleWithdrew(event: WithdrewEvent): void {
+  const entity = new Withdraw(
+    event.transaction.hash.toHexString() + event.logIndex.toString()
+  );
+  entity.transactionHash = event.transaction.hash;
+  entity.logIndex = event.logIndex;
+  entity.blockNumber = event.block.number;
+  entity.user = event.params.user;
+  entity.amount = event.params.amount;
+  entity.expirationTimestamp = event.params.expirationTimestamp;
+  entity.withdrawalHash = event.params.withdrawalHash;
+  entity.withdrawalSigner = event.params.withdrawalSigner;
+  entity.withdrawalDestination = event.params.withdrawalDestination;
+  entity.withdrawalLimit = event.params.withdrawalLimit;
 
   entity.save();
 }
