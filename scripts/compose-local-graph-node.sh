7ac83bce28a6569e81e1d8a54e3d88c97f7b1cec
Szük Dániel <daniel.szuk@vacuumlabs.com>
2023-08-30 16:05:56 +0200

    add api3-subgraphs as a dependency (#4)
    * add docker management scripts
    
    * support generate-abi, while  api3-subgraphs is a dependency

--- scripts/compose-local-graph-node.sh
+++
@@ -1,3 +0,0 @@
-cd "$(dirname "$0")" # .bin directory
-cd "$(dirname "$(realpath compose-local-graph-node)")" # homedir of command (have to match the name in package.json > bin)
-yarn compose-local-graph-node
